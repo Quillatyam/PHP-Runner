61174f014b9cec2c74d4f22b8031ca732f179d25
Jasper de Vries <quillatyam@gmail.com>
2017-06-13 10:30:46 +0200

    Add default index.php

--- src/index.php
+++ src/index.php
@@ -1,3 +1,5 @@
 <?php
 
-echo "Hallo";
+echo "<h1>Hello PHP-Runner</h1>";
+echo "<p>If you are seeing this page, it means <strong>PHP-Runner</strong> is working, yay!</p>";
+echo "<p>To run your own code, just edit <code>./src/index.php</code> to run whatever you like in PHP!</p>";
